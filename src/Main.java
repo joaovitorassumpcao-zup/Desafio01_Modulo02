c1d88e4e7505d2d15575f0962ea2c41a45f3c50e
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-06-06 18:26:23 -0300

    Difficulty

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,51 @@ public class Main {
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        int score = 0;
+        int score = 0, bound;
 
-        System.out.println("Tente adivinhar o número aleatório!  (1 - 10)  Para parar digite '0'");
+        System.out.println("_____Tente Adivinhar o Número Aleatório_____");
+        System.out.println("Escolha a Dificuldade: \n" +
+                "1 -> (1 - 5)\n" +
+                "2 -> (1 - 10)\n" +
+                "3 -> (1 - 20)");
+        int difficulty = scanner.nextInt();
+
+        switch (difficulty) {
+            case 1:
+                bound = 4;
+                break;
+            case 2:
+                bound = 9;
+                break;
+            case 3:
+                bound = 19;
+                break;
+            default:
+                System.out.println("Opção Inválida!");
+                return;
+        }
+
+        System.out.println("Para parar digite '0'");
 
         while (true) {
             System.out.print(" Seu número: ");
             int num = scanner.nextInt();
             if (num == 0) break;
 
-            int randNum = random.nextInt(9) + 1;
+            int randNum = random.nextInt(bound) + 1;
 
             System.out.println(" Meu número: " + randNum);
             if (num == randNum) {
-                System.out.println("Acertou! Ganha 10 pontos!");
+                System.out.print("Acertou! Ganha 10 pontos!   ");
                 score += 10;
             } else if (num == randNum + 1 || num == randNum - 1) {
-                System.out.println("Por pouco! Ganha 5 pontos");
+                System.out.print("Por pouco! Ganha 5 pontos.  ");
                 score += 5;
             } else {
-                System.out.println("Não acertou. Tente novamente");
+                System.out.print("Não acertou. Tente novamente");
             }
 
-            System.out.println("Seus pontos: " + score);
+            System.out.println("        Seus pontos: " + score);
         }
     }
 }
