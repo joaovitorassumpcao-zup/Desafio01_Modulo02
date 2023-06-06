6e9b8830e4e4e4cdb1f2e9fb9f4ecb295da3d2c4
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-06-06 16:09:32 -0300

    Score

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
-import org.w3c.dom.ranges.Range;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void roundCheck
-
     public static void main(String[] args) {
+
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        int score = 0;
 
-        System.out.println("Tente adivinhar o número aleatório!  (1 - 20)" +
-                "\nPara parar digite '0'");
+        System.out.println("Tente adivinhar o número aleatório!  (1 - 10)  Para parar digite '0'");
+
         while (true) {
             System.out.print(" Seu número: ");
             int num = scanner.nextInt();
-            int randNum = random.nextInt(19) + 1;
+            if (num == 0) break;
+
+            int randNum = random.nextInt(9) + 1;
 
             System.out.println(" Meu número: " + randNum);
             if (num == randNum) {
                 System.out.println("Acertou! Ganha 10 pontos!");
+                score += 10;
             } else if (num == randNum + 1 || num == randNum - 1) {
                 System.out.println("Por pouco! Ganha 5 pontos");
+                score += 5;
             } else {
                 System.out.println("Não acertou. Tente novamente");
             }
+
+            System.out.println("Seus pontos: " + score);
         }
     }
 }
